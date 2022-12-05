544e280cc48cf2f5697f9151cf0df8fd1c40e65e
jcdavidarg <jcdavidarg@gmail.com>
2022-12-04 21:57:27 -0300

    Revert "componentes de clase"
    This reverts commit 704ce2081cc2046fd5a918e9e0bee50e7e58fb48.

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "../stylesheets/Testimonio.css";
 
-class Testimonio extends React.Component {
-  render() {
-    return (
-      <div className="contenedor-testimonio">
-        <img
-          className="imagen-testimonio"
-          src={require(`../images/testimonio-${this.props.imagen}.png`)}
-          alt={`Foto de ${this.props.imagen}`}
-        />
-        <div className="contenedor-texto-testimonio">
-          <p className="nombre-testimonio">
-            <strong>{this.props.nombre}</strong> en {this.props.pais}
-          </p>
-          <p className="cargo-testimonio">
-            {this.props.cargo} en <strong>{this.props.empresa}</strong>
-          </p>
-          <p className="texto-testimonio">"{this.props.testimonio}"</p>
-        </div>
+const Testimonio = (props) => {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../images/testimonio-${props.imagen}.png`)}
+        alt={`Foto de ${props.imagen}`}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Testimonio;
