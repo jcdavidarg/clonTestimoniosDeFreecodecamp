d458e13ec3be891644f71bdbd8e33e3a67017e2e
jcdavidarg <jcdavidarg@gmail.com>
2022-12-04 21:52:35 -0300

    componentes de clase

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import "./App.css";
 import Testimonios from "./components/Testimonios";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="contenedor-principal">
-          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-          <Testimonios />
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonios />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
